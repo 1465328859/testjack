d719ac6bcc76c88a4602b1fb3522c0d76b979d4c
bigjack <1465328859@qq.com>
2020-09-02 15:33:27 +0800

    抓取cnproxy.com代理数据

--- main.py
+++ main.py
@@ -2,15 +2,24 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from lxml import html
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+def report(url):
+    proxy = requests.get("http://cn-proxy.com/")
+    proxytext=html.fromstring(proxy.text)
+    index = 1
+    while (index):
+        xpath='//*[@id="post-4"]/div/div[4]/table/tbody/tr['+str(index)+']/td/text()'
+        proxytable=proxytext.xpath(xpath)
+        index+=1
+        if proxytable:
+            print(proxytable)
+        else:
+            break
+    '//*[@id="post-4"]/div/div[4]/table/tbody/tr[1]'
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    report('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
